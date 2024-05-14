0e82bca1b893ff07dea241246d55751f5974f2bf
Ernesto Fernandez <fernandez.ernes@gmail.com>
2024-05-14 17:16:47 -0300

    Se agregaron los archivos csv y la clase CSVReader al proyecto.

---
+++ src/Servicios.java
@@ -0,0 +1,45 @@
+package src;
+
+import utils.CSVReader;
+
+import java.util.List;
+
+/**
+ * NO modificar la interfaz de esta clase ni sus métodos públicos.
+ * Sólo se podrá adaptar el nombre de la clase "Tarea" según sus decisiones
+ * de implementación.
+ */
+public class Servicios {
+
+	/*
+     * Expresar la complejidad temporal del constructor.
+     */
+	public Servicios(String pathProcesadores, String pathTareas)
+	{
+		CSVReader reader = new CSVReader();
+		reader.readProcessors(pathProcesadores);
+		reader.readTasks(pathTareas);
+	}
+	
+	/*
+     * Expresar la complejidad temporal del servicio 1.
+     */
+	public Tarea servicio1(String ID) {
+		return null;
+	}
+    
+    /*
+     * Expresar la complejidad temporal del servicio 2.
+     */
+	public List<Tarea> servicio2(boolean esCritica) {
+		return null;
+	}
+
+    /*
+     * Expresar la complejidad temporal del servicio 3.
+     */
+	public List<Tarea> servicio3(int prioridadInferior, int prioridadSuperior) {
+		return null;
+	}
+
+}
